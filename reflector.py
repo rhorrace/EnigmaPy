67ef9602ed5f60f034f8a4ae9def86f9bc52ea1a
Robert Horrace <rhorrace95@gmail.com>
2023-01-22 19:34:39 -0500

    Delete reflector.py

--- reflector.py
+++
@@ -1,9 +0,0 @@
-from constants import LETTERS
-
-
-class Reflector:
-    def __init__(self, letters=LETTERS):
-        self.wiring = dict(zip(LETTERS, letters))
-
-    def reflect(self, letter):
-        return self.wiring[letter]
